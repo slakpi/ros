78e09e891aaa4d8f0d39390b3bf6a6c5c0b2aa55
Randy Widell <randy@slakr.org>
2023-01-02 17:41:37 -0800

    Added trap output.

--- src/ros_kernel/src/exceptions/trap.rs
+++ src/ros_kernel/src/exceptions/trap.rs
@@ -1,6 +1,10 @@
+use crate::{dbg_print};
+
 /// @fn trap_exception(esr_el1: usize, far_el1: usize)
 /// @brief Handles an exception trap.
 /// @param[in] esr_el1 Exception Syndrome Register.
 /// @param[in] far_el1 Fault Address Register.
 #[no_mangle]
-pub extern "C" fn trap_exception(_esr_el1: usize, _far_el1: usize) {}
+pub extern "C" fn trap_exception(esr_el1: usize, far_el1: usize) {
+  dbg_print!("Fell into a trap! esr_el1={:#x}, far_el1={:#x}\n", esr_el1, far_el1);
+}
