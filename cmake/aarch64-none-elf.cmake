b712cc921db757a57199039464c87f128d914b62
Randy Widell <randy@slakr.org>
2023-01-02 17:26:54 -0800

    Simplified linker scripts.

--- cmake/aarch64-none-elf.cmake
+++ cmake/aarch64-none-elf.cmake
@@ -35,9 +35,9 @@ set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "")
 # not allow floating-point or vector instructions.
 set(Rust_CARGO_TARGET aarch64-unknown-none-softfloat)
 
+set(ROS_KERNEL_IMAGE_FILE kernel8.img)
+
 # QEMU_BUILD is not used by AArch64. It has no effect, so go ahead and just
 # quiet the warning about it being unused.
 set(ignore_QEMU_BUILD ${QEMU_BUILD})
-
-set(ROS_KERNEL_IMAGE_FILE kernel8.img)
-set(ROS_KERNEL_LINK_SCRIPT boot.ld)
+set(ROS_KERNEL_BASE_ADDRESS 0x80000)
