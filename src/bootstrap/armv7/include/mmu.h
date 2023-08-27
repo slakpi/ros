22be9d218c85f745afc152ddd4df9e3708f59555
Randy Widell <randy@slakr.org>
2023-08-27 15:59:43 -0700

    Fixed access permissions.

--- src/bootstrap/armv7/include/mmu.h
+++ src/bootstrap/armv7/include/mmu.h
@@ -8,13 +8,16 @@
 // virtual address are 0, TTBR0 is used. Otherwise, TTBR1 is used.
 #define TTBCR_VALUE 0x2
 
-// Page descriptor flags. See B3.5.1.
+// Page descriptor flags. See B3.5.1. The Access Flag Enable bit should be set
+// to 1 in SCTLR to use the 2-bit access control model where AP[2] (bit 15)
+// disables writing and A[1] (bit 11) enables unpriviledged access. AP[0] (bit
+// 10) is the Access Flag.
 #define MM_TYPE_PAGE_TABLE 0x1
 #define MM_TYPE_PAGE       0x2
 #define MM_TYPE_BLOCK      0x2
-#define MM_ACCESS_FLAG     (1 << 15)
-#define MM_ACCESS_RW       (0b00 << 10)
-#define MM_ACCESS_RO       (0b10 << 10)
+#define MM_ACCESS_FLAG     (0b1 << 10)
+#define MM_ACCESS_RW       (0b0 << 15)
+#define MM_ACCESS_RO       (0b1 << 15)
 #define MM_DEVICE_CB       (0b01 << 2)
 #define MM_NORMAL_CB       (0b10 << 2)
 
@@ -22,12 +25,5 @@
 #define MMU_NORMAL_RW_FLAGS (MM_TYPE_BLOCK | MM_ACCESS_RW | MM_NORMAL_CB | MM_ACCESS_FLAG)
 #define MMU_DEVICE_RO_FLAGS (MM_TYPE_BLOCK | MM_ACCESS_RO | MM_DEVICE_CB | MM_ACCESS_FLAG)
 #define MMU_DEVICE_RW_FLAGS (MM_TYPE_BLOCK | MM_ACCESS_RW | MM_DEVICE_CB | MM_ACCESS_FLAG)
-
-// SCTLR bit to enable the MMU. See B4.1.130.
-#define MMU_ENABLE 0x1
-
-// DACR setup. See B4.1.43. Only using domain 0 in client mode (access
-// permissions are checked).
-#define DACR_VALUE 0x3
 
 #endif
