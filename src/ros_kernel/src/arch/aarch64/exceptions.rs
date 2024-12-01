c78d93341f158ecef4d9f7c7f341e2b014910fd8
Randy Widell <randy@slakr.org>
2024-11-30 17:55:36 -0800

    Cleaning up exception entry/exit.

--- src/ros_kernel/src/arch/aarch64/exceptions.rs
+++ src/ros_kernel/src/arch/aarch64/exceptions.rs
@@ -2,14 +2,61 @@
 
 use crate::debug_print;
 
+/// The CPU context upon exception entry. See `exceptions.S` regarding floating-
+/// point registers.
+#[repr(C)]
+struct CpuContext {
+  x0: usize,
+  x1: usize,
+  x2: usize,
+  x3: usize,
+  x4: usize,
+  x5: usize,
+  x6: usize,
+  x7: usize,
+  x8: usize,
+  x9: usize,
+  x10: usize,
+  x11: usize,
+  x12: usize,
+  x13: usize,
+  x14: usize,
+  x15: usize,
+  x16: usize,
+  x17: usize,
+  x18: usize,
+
+  x19: usize,
+  x20: usize,
+  x21: usize,
+  x22: usize,
+  x23: usize,
+  x24: usize,
+  x25: usize,
+  x26: usize,
+  x27: usize,
+  x28: usize,
+  x29: usize,
+  x30: usize,
+  sp: usize,
+  
+  elr_el1: usize,
+  spsr_el1: usize,
+}
+
 /// AArch64 exception trap.
 ///
 /// # Parameters
 ///
 /// * `esr_el1` - Exception Syndrome Register value.
 /// * `far_el1` - Fault Address Register value.
+/// * `cpu_context` - Pointer to the saved CPU context structure.
 #[no_mangle]
-extern "C" fn trap_exception(esr_el1: usize, far_el1: usize) {
+extern "C" fn trap_exception(esr_el1: usize, far_el1: usize, cpu_context: usize) {
+  assert!(cpu_context != 0);
+  
+  _ = unsafe { &*(cpu_context as *const CpuContext) };
+
   debug_print!(
     "Fell into a trap! esr_el1={:#x}, far_el1={:#x}\n",
     esr_el1,
--- src/ros_kernel/src/arch/aarch64/task.rs
+++ src/ros_kernel/src/arch/aarch64/task.rs
@@ -1,4 +1,4 @@
-struct CpuContext {
+struct TaskContext {
   x19: usize,
   x20: usize,
   x21: usize,
@@ -9,21 +9,21 @@ struct CpuContext {
   x26: usize,
   x27: usize,
   x28: usize,
-  fp: usize, // x29, the frame pointer
-  pc: usize, // x30, the link register
-  sp: usize, // x31, the stack pointer
+  x29: usize, // the frame pointer
+  x30: usize, // the link register
+  sp: usize,  // the stack pointer
 }
 
 pub struct Task {
   task_id: usize,
-  cpu_context: CpuContext,
+  context: TaskContext,
 }
 
 impl Task {
   pub const fn new(task_id: usize) -> Self {
     Task {
       task_id,
-      cpu_context: CpuContext {
+      context: TaskContext {
         x19: 0,
         x20: 0,
         x21: 0,
@@ -34,8 +34,8 @@ impl Task {
         x26: 0,
         x27: 0,
         x28: 0,
-        fp: 0,
-        pc: 0,
+        x29: 0,
+        x30: 0,
         sp: 0,
       },
     }
